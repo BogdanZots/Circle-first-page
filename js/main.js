fffb2a6a20ab888c7fdbd08b7c0a91f2487f471b
BogdanZots <bzots@ukr.net>
2021-01-21 13:03:08 +0300

    1

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ $('.menu-button').on('click', function(e) {
 	e.preventDefault();
  $('.hidden-header__menu').toggleClass('visible');
 });
+let buyTicketsBtn = document.getElementById('.buy-tickets');
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert('mobile')
+  } else {
+   alert('pc')
+}
